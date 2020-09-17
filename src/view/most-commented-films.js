6aa6241b6dc998133a7386062306161112f4878b
jhonysmith <evesnin68@gmail.com>
2020-09-17 22:40:31 +0300

    Разобьет main.js на модули

---
+++ src/view/most-commented-films.js
@@ -0,0 +1,10 @@
+export const createMostCommentedFilmsTemplate = () => {
+  return (
+    `<section class="films-list--extra most-commented">
+      <h2 class="films-list__title">Top rated</h2>
+
+      <div class="films-list__container">
+      </div>
+    </section>`
+  );
+};
